b52554ebc2110a0334cf35a2877c65ead9150077
Leopold Luley <git@leopoldluley.de>
2020-11-18 20:17:11 +0100

    Add basic structures and methods.

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,44 @@
 
 pub struct Client {}
+
+pub struct Experiment<'a> {
+    client: &'a Client,
+    name: String,
+}
+
+pub struct Run<'a> {
+    experiment: &'a Experiment<'a>,
+}
+
+impl Client {
+    pub fn create_experiment(&self, name: &str) -> Experiment {
+        Experiment {
+            client: self,
+            name: name.to_string(),
+        }
+    }
+    pub fn get_experiment(&self, name: &str) -> Option<Experiment> {
+        None
+    }
+    pub fn list_experiments(&self) -> Vec<Experiment> {
+        Vec::new()
+    }
+}
+
+impl Experiment<'_> {
+    pub fn create_run(&self) -> Run {
+        Run {
+            experiment: self,
+        }
+    }
+
+    pub fn name(&self) -> &str {
+        &self.name
+    }
+}
+
+impl Run<'_> {
+    pub fn log_param(&self, name: &str, value: &str) {}
+    pub fn log_metric(&self, name: &str, value: f64) {}
+    pub fn terminate(self) {}
+}
